abc56634edd1c3935f1d255254cf5fccdbf2555d
Shi Lei <shi_lei@massclouds.com>
2023-12-01 10:41:45 +0800

    parse app elf

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -2,7 +2,7 @@
 #![cfg_attr(feature = "axstd", no_main)]
 
 #[cfg(feature = "axstd")]
-use axstd::println;
+use axstd::{println, thread};
 
 extern crate alloc;
 use alloc::string::String;
@@ -11,10 +11,32 @@ const PFLASH_START: usize = 0xffff_ffc0_2200_0000;
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
-    let app_num = parse_literal_hex(PFLASH_START);
+    let mut pos = PFLASH_START;
+    let app_num = parse_literal_hex(pos);
     assert_eq!(app_num, 2);
+    pos += 8;
 
-    println!("Hello, world!");
+    let size = parse_literal_hex(pos);
+    println!("app size: {}", size);
+    pos += 8;
+
+    let code = unsafe {
+        core::slice::from_raw_parts(pos as *const u8, size)
+    };
+    pos += size;
+    println!("app pos: {:#X}", pos);
+
+    thread::spawn(move || {
+        let (entry, end) = parse_elf(code);
+        println!("elf entry: {:#X}", entry);
+
+        println!("App {:?}", thread::current().id());
+        run_app(entry, end);
+    });
+
+    loop {
+        thread::yield_now();
+    }
 }
 
 // Note: Length of literal hex must be 8.
@@ -22,4 +44,12 @@ fn parse_literal_hex(pos: usize) -> usize {
     let hex = unsafe { core::slice::from_raw_parts(pos as *const u8, 8) };
     let hex = String::from_utf8(hex.into()).expect("bad hex number.");
     usize::from_str_radix(&hex, 16).expect("NOT hex number.")
+}
+
+fn parse_elf(_code: &[u8]) -> (usize, usize) {
+    unimplemented!("parse_elf");
+}
+
+fn run_app(_entry: usize, _end: usize) {
+    unimplemented!("run_app");
 }
