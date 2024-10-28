099ebc3e4eb3636841f13eac44a1645df47b6125
Shi Lei <shi_lei@massclouds.com>
2024-10-28 01:04:29 +0000

    fix origin app

--- payload/origin/src/main.rs
+++ payload/origin/src/main.rs
@@ -6,14 +6,6 @@ use core::panic::PanicInfo;
 #[no_mangle]
 unsafe extern "C" fn _start() -> ! {
     core::arch::asm!(
-        "ebreak",
-"1:",
-        "li a7, 63",
-        "ecall",
-        "li t0, 1",
-        "ble a0, t0, 1b",
-        "li a7, 64",
-        "ecall",
         "li a7, 93",
         "ecall",
         options(noreturn)
