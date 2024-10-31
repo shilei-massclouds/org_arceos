4e1111dc0540732cf79030089b6ca53d7c6afcf0
Shi Lei <shi_lei@massclouds.com>
2024-10-31 13:39:38 +0000

    fix h_1_0

--- tour/h_1_0/src/main.rs
+++ tour/h_1_0/src/main.rs
@@ -80,9 +80,11 @@ fn main() {
         core::arch::riscv64::hfence_gvma_all();
     }
 
-    unsafe {
-        _run_guest(&mut ctx);
+    for i in 0..4 {
+        run_guest(&mut ctx);
+        println!("vm exit [{}]", i);
     }
+
     /*
     let user_task = task::spawn_user_task(
         Arc::new(Mutex::new(uspace)),
@@ -96,68 +98,14 @@ fn main() {
     panic!("Hypervisor ok!");
 }
 
-/*
-extern crate alloc;
-
-mod task;
-mod syscall;
-
-use axhal::arch::UspaceContext;
-use axsync::Mutex;
-use alloc::sync::Arc;
-
-const USER_STACK_SIZE: usize = 0x10000;
-const KERNEL_STACK_SIZE: usize = 0x40000; // 256 KiB
-
-#[cfg_attr(feature = "axstd", no_mangle)]
-fn main() {
-    let mut buf = [0u8; 64];
-    if let Err(e) = load_user_app("/sbin/origin.bin", &mut buf) {
-        panic!("Cannot load app! {:?}", e);
-    }
-
-    let entry = 0x1000;
-    let mut uspace = axmm::new_user_aspace().unwrap();
-    uspace.map_alloc(entry.into(), PAGE_SIZE_4K, MappingFlags::READ|MappingFlags::WRITE|MappingFlags::EXECUTE|MappingFlags::USER, true).unwrap();
-
-    let (paddr, _, _) = uspace
-        .page_table()
-        .query(entry.into())
-        .unwrap_or_else(|_| panic!("Mapping failed for segment: {:#x}", entry));
-
-    println!("paddr: {:#x}", paddr);
-
+fn run_guest(ctx: &mut VmCpuRegisters) {
     unsafe {
-        core::ptr::copy_nonoverlapping(
-            buf.as_ptr(),
-            phys_to_virt(paddr).as_mut_ptr(),
-            PAGE_SIZE_4K,
-        );
+        _run_guest(ctx);
     }
 
-    let ustack_top = uspace.end();
-    let ustack_vaddr = ustack_top - crate::USER_STACK_SIZE;
-    println!(
-        "Mapping user stack: {:#x?} -> {:#x?}",
-        ustack_vaddr, ustack_top
-    );
-    uspace.map_alloc(
-        ustack_vaddr,
-        crate::USER_STACK_SIZE,
-        MappingFlags::READ | MappingFlags::WRITE | MappingFlags::USER,
-        true,
-    ).unwrap();
-    println!("New user address space: {:#x?}", uspace);
-
-    let user_task = task::spawn_user_task(
-        Arc::new(Mutex::new(uspace)),
-        UspaceContext::new(entry.into(), ustack_top, 2333),
-    );
-    let exit_code = user_task.join();
-
-    println!("monolithic kernel exit [{:?}] normally!", exit_code);
+    let scause = scause::read();
+    println!("vm exit reason: {:?}", scause.cause());
 }
-*/
 
 fn load_user_app(fname: &str, buf: &mut [u8]) -> io::Result<usize> {
     println!("app: {}", fname);
