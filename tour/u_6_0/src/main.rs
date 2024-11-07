3046b80767c52f17a39c4e122995eaab4d8d4e54
Shi Lei <shi_lei@massclouds.com>
2024-11-07 01:07:00 +0000

    fix: use ax_println rather than println, the former has lock

--- tour/u_6_0/src/main.rs
+++ tour/u_6_0/src/main.rs
@@ -1,53 +1,54 @@
 #![cfg_attr(feature = "axstd", no_std)]
 #![cfg_attr(feature = "axstd", no_main)]
 
-#[macro_use]
 #[cfg(feature = "axstd")]
 extern crate axstd as std;
+#[macro_use]
+extern crate axlog;
 
 use std::thread;
 use std::collections::VecDeque;
 use std::sync::Arc;
-use std::os::arceos::modules::axsync::Mutex;
+use std::os::arceos::modules::axsync::spin::SpinNoIrq;
 
 const LOOP_NUM: usize = 256;
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
-    println!("Multi-task(Preemptible) is starting ...");
+    ax_println!("Multi-task(Preemptible) is starting ...");
 
-    let q1 = Arc::new(Mutex::new(VecDeque::new()));
+    let q1 = Arc::new(SpinNoIrq::new(VecDeque::new()));
     let q2 = q1.clone();
 
     let worker1 = thread::spawn(move || {
-        println!("worker1 ...");
+        ax_println!("worker1 ... {:?}", thread::current().id());
         for i in 0..=LOOP_NUM {
-            println!("worker1 [{i}]");
+            ax_println!("worker1 [{i}]");
             q1.lock().push_back(i);
         }
-        println!("worker1 ok!");
+        ax_println!("worker1 ok!");
     });
 
     let worker2 = thread::spawn(move || {
-        println!("worker2 ...");
+        ax_println!("worker2 ... {:?}", thread::current().id());
         loop {
             if let Some(num) = q2.lock().pop_front() {
-                println!("worker2 [{num}]");
+                ax_println!("worker2 [{num}]");
                 if num == LOOP_NUM {
                     break;
                 }
             } else {
-                println!("worker2: nothing to do!");
+                ax_println!("worker2: nothing to do!");
                 // TODO: it should sleep and wait for notify!
                 thread::yield_now();
             }
         }
-        println!("worker2 ok!");
+        ax_println!("worker2 ok!");
     });
 
-    println!("Wait for workers to exit ...");
+    ax_println!("Wait for workers to exit ...");
     let _ = worker1.join();
     let _ = worker2.join();
 
-    println!("Multi-task(Preemptible) ok!");
+    ax_println!("Multi-task(Preemptible) ok!");
 }
--- tour/u_6_1/src/main.rs
+++ tour/u_6_1/src/main.rs
@@ -1,15 +1,15 @@
 #![cfg_attr(feature = "axstd", no_std)]
 #![cfg_attr(feature = "axstd", no_main)]
 
-#[macro_use]
 #[cfg(feature = "axstd")]
 extern crate axstd as std;
+#[macro_use]
+extern crate axlog;
 
 use std::thread;
 use std::collections::VecDeque;
 use std::sync::Arc;
-use std::time::Duration;
-use std::os::arceos::modules::axsync::Mutex;
+use std::sync::Mutex;
 use std::os::arceos::modules::axtask::WaitQueue;
 
 const LOOP_NUM: usize = 256;
@@ -17,40 +17,40 @@ static WQ: WaitQueue = WaitQueue::new();
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
-    println!("WaitQ is starting ...");
+    ax_println!("WaitQ is starting ...");
 
     let q1 = Arc::new(Mutex::new(VecDeque::new()));
     let q2 = q1.clone();
 
     let worker1 = thread::spawn(move || {
-        println!("worker1 ...");
+        ax_println!("worker1 ...");
         for i in 0..=LOOP_NUM {
-            println!("worker1 [{i}]");
+            ax_println!("worker1 [{i}]");
             q1.lock().push_back(i);
             WQ.notify_one(true);
         }
-        println!("worker1 ok!");
+        ax_println!("worker1 ok!");
     });
 
     let worker2 = thread::spawn(move || {
-        println!("worker2 ...");
+        ax_println!("worker2 ...");
         loop {
             if let Some(num) = q2.lock().pop_front() {
-                println!("worker2 [{num}]");
+                ax_println!("worker2 [{num}]");
                 if num == LOOP_NUM {
                     break;
                 }
-            } else {
-                println!("worker2: nothing to do!");
-                WQ.wait_timeout(Duration::new(0, 5000));
+                continue;
             }
+
+            WQ.wait();
         }
-        println!("worker2 ok!");
+        ax_println!("worker2 ok!");
     });
 
-    println!("Wait for workers to exit ...");
+    ax_println!("Wait for workers to exit ...");
     let _ = worker1.join();
     let _ = worker2.join();
 
-    println!("WaitQ ok!");
+    ax_println!("WaitQ ok!");
 }
