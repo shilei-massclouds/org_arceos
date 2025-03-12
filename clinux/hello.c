7cb30d24fc27a2de2586a067108554034c1222c6
Shi Lei <shi_lei@massclouds.com>
2025-03-12 08:08:46 +0000

    clinux: add mine

--- clinux/hello.c
+++ clinux/hello.c
@@ -1,8 +1,96 @@
+#include "booter.h"
+
 void sbi_console_putchar(int ch);
+void cl_virtio_blk_init();
+
+// NOTE: Don't expose these global vars.
+//struct page *mem_map;
+void *mem_map = 0;
+unsigned long pfn_base;
+unsigned long va_pa_offset;
+int kmalloc_caches[1];
 
 int say_hello()
 {
-    sbi_console_putchar('B');
+    sbi_console_putchar('D');
+    cl_virtio_blk_init();
     sbi_console_putchar('\n');
-    return 202;
+    booter_panic("ok!");
+    return 303;
+}
+
+void sbi_puts(const char *s)
+{
+    for (; *s; s++) {
+        if (*s == '\n')
+            sbi_console_putchar('\r');
+        sbi_console_putchar(*s);
+    }
+}
+
+void sbi_put_u64(unsigned long n)
+{
+    char buf[UL_STR_SIZE];
+    hex_to_str(n, buf, sizeof(buf));
+    sbi_puts(buf);
+}
+
+void sbi_put_dec(unsigned long n)
+{
+    char buf[UL_STR_SIZE];
+    dec_to_str(n, buf, sizeof(buf));
+    sbi_puts(buf);
+}
+
+int hex_to_str(unsigned long n, char *str, size_t len)
+{
+    /* prefix with '0x' and end with '\0' */
+    if (len != 19)
+        return -1;
+
+    str[0] = '0';
+    str[1] = 'x';
+    for (int i = 3; i <= 18; i++) {
+        char c = (n >> ((18 - i)*4)) & 0xF;
+        if (c >= 10) {
+            c -= 10;
+            c += 'A';
+        } else {
+            c += '0';
+        }
+        str[i-1] = c;
+    }
+    str[18] = '\0';
+
+    return 0;
+}
+
+int dec_to_str(unsigned long n, char *str, size_t len)
+{
+    int i;
+    int pos = 0;
+    char stack[10];
+
+    if (len < 10)
+        return -1;
+
+    while (1) {
+        stack[pos] = '0' + n % 10;
+        if (n < 10) {
+            break;
+        }
+        pos++;
+        n /= 10;
+    }
+
+    for (i = 0; i <= pos; i++) {
+        str[i] = stack[pos - i];
+    }
+    str[i] = '\0';
+    return 0;
+}
+
+int snprintf(char *buf, size_t size, const char *fmt, ...)
+{
+    booter_panic("No impl.\n");
 }
