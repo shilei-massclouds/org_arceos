21acc63bdc3f0ecdc9a1b3ecbeefdcee48c8ba04
Shi Lei <shi_lei@massclouds.com>
2025-06-12 15:40:36 +0000

    fix: support arch for linux_adaptor

--- modules/linux_adaptor/build.rs
+++ modules/linux_adaptor/build.rs
@@ -2,9 +2,14 @@ use std::env;
 use std::process::Command;
 
 fn main() {
+    let arch = env::var("AX_ARCH").unwrap();
     let root_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let cwd = root_dir + "/kernel_modules";
-    let output = Command::new("make").current_dir(&cwd).output().expect("Make error.");
+    let output = Command::new("make")
+        .current_dir(&cwd)
+        .env("ARCH", arch)
+        .output()
+        .expect("Make error.");
     if !output.status.success() {
         let err = String::from_utf8(output.stderr).unwrap();
         panic!("{}", err);
