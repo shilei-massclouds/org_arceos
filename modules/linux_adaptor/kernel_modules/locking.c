453ab794efe73295a23ea9d9c45e5c9ba8ea0050
Shi Lei <shi_lei@massclouds.com>
2025-06-15 08:30:45 +0000

    spinlock: impl it

--- modules/linux_adaptor/kernel_modules/locking.c
+++ modules/linux_adaptor/kernel_modules/locking.c
@@ -6,18 +6,26 @@
 void __raw_spin_lock_init(raw_spinlock_t *lock, const char *name,
               struct lock_class_key *key, short inner)
 {
-    log_error("%s: WARN: impl it. (%lx) (%s)\n",
-           __func__, (unsigned long)lock, name);
+    lock->raw_lock = (arch_spinlock_t)__ARCH_SPIN_LOCK_UNLOCKED;
+    lock->magic = SPINLOCK_MAGIC;
+    lock->owner = SPINLOCK_OWNER_INIT;
+    lock->owner_cpu = -1;
 }
 
 void __lockfunc _raw_spin_lock(raw_spinlock_t *lock)
 {
-    log_error("%s: ===> WARN: impl it. (%lx)\n", __func__, (unsigned long)lock);
+    /* For simplicity, ignore kernel-preemption. */
+    // preempt_disable();
+
+    arch_spin_lock(&lock->raw_lock);
 }
 
 void __lockfunc _raw_spin_unlock(raw_spinlock_t *lock)
 {
-    log_error("%s: ===> WARN: impl it. (%lx)\n", __func__, (unsigned long)lock);
+    arch_spin_unlock(&lock->raw_lock);
+
+    /* For simplicity, ignore kernel-preemption. */
+    // preempt_enable();
 }
 
 unsigned long __lockfunc _raw_spin_lock_irqsave(raw_spinlock_t *lock)
