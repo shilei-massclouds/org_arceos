f321a5e01638127870b0ba04546a56e88418cb9a
Shi Lei <shi_lei@massclouds.com>
2025-07-24 14:40:07 +0000

    test read block

---
+++ modules/linux_adaptor/kernel_modules/block/fops.c
@@ -0,0 +1,85 @@
+#include <linux/init.h>
+#include <linux/mm.h>
+#include <linux/blkdev.h>
+#include <linux/buffer_head.h>
+#include <linux/mpage.h>
+#include <linux/uio.h>
+#include <linux/namei.h>
+#include <linux/task_io_accounting_ops.h>
+#include <linux/falloc.h>
+#include <linux/suspend.h>
+#include <linux/fs.h>
+#include <linux/iomap.h>
+#include <linux/module.h>
+#include <linux/io_uring/cmd.h>
+#include "blk.h"
+
+static int blkdev_get_block(struct inode *inode, sector_t iblock,
+		struct buffer_head *bh, int create)
+{
+	bh->b_bdev = I_BDEV(inode);
+	bh->b_blocknr = iblock;
+	set_buffer_mapped(bh);
+	return 0;
+}
+
+/*
+ * We cannot call mpage_writepages() as it does not take the buffer lock.
+ * We must use block_write_full_folio() directly which holds the buffer
+ * lock.  The buffer lock provides the synchronisation with writeback
+ * that filesystems rely on when they use the blockdev's mapping.
+ */
+static int blkdev_writepages(struct address_space *mapping,
+		struct writeback_control *wbc)
+{
+	struct blk_plug plug;
+	int err;
+
+	blk_start_plug(&plug);
+	err = write_cache_pages(mapping, wbc, block_write_full_folio,
+			blkdev_get_block);
+	blk_finish_plug(&plug);
+
+	return err;
+}
+
+static int blkdev_read_folio(struct file *file, struct folio *folio)
+{
+	return block_read_full_folio(folio, blkdev_get_block);
+}
+
+static void blkdev_readahead(struct readahead_control *rac)
+{
+	mpage_readahead(rac, blkdev_get_block);
+}
+
+static int blkdev_write_begin(struct file *file, struct address_space *mapping,
+		loff_t pos, unsigned len, struct folio **foliop, void **fsdata)
+{
+	return block_write_begin(mapping, pos, len, foliop, blkdev_get_block);
+}
+
+static int blkdev_write_end(struct file *file, struct address_space *mapping,
+		loff_t pos, unsigned len, unsigned copied, struct folio *folio,
+		void *fsdata)
+{
+	int ret;
+	ret = block_write_end(file, mapping, pos, len, copied, folio, fsdata);
+
+	folio_unlock(folio);
+	folio_put(folio);
+
+	return ret;
+}
+
+const struct address_space_operations def_blk_aops = {
+	.dirty_folio	= block_dirty_folio,
+	.invalidate_folio = block_invalidate_folio,
+	.read_folio	= blkdev_read_folio,
+	.readahead	= blkdev_readahead,
+	.writepages	= blkdev_writepages,
+	.write_begin	= blkdev_write_begin,
+	.write_end	= blkdev_write_end,
+	.migrate_folio	= buffer_migrate_folio_norefs,
+	.is_dirty_writeback = buffer_check_dirty_writeback,
+};
--- modules/linux_adaptor/kernel_modules/test_block.c
+++ modules/linux_adaptor/kernel_modules/test_block.c
@@ -4,10 +4,38 @@
 
 void test_block(void)
 {
+    int ret;
+    const struct address_space_operations *a_ops;
     struct block_device *dev;
+
     dev = blkdev_get_no_open(MKDEV(0xFE, 0x00));
-    if (dev == NULL) {
+    if (dev == NULL || dev->bd_mapping == NULL) {
         PANIC("No block device!");
     }
+
+    a_ops = dev->bd_mapping->a_ops;
+    if (a_ops == NULL) {
+        PANIC("No bdev page cache ops!");
+    }
+
+    /*
+     * Read block tests
+     */
+    if (a_ops->read_folio == NULL) {
+        PANIC("No 'read_folio'.");
+    }
+
+    struct page *page = alloc_page(GFP_KERNEL);
+    if (page == NULL) {
+        PANIC("No page.");
+    }
+
+    /* Read first block (PAGE_SIZE) */
+    page->index = 0;
+    ret = a_ops->read_folio(NULL, page);
+    if (ret) {
+        pr_err("Read block err: %d\n", ret);
+    }
+
     PANIC("Test block ok!");
 }
