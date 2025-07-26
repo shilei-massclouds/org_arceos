9d94e7f8cf5e67fbd5fca79cc9c9b56218f48792
Shi Lei <shi_lei@massclouds.com>
2025-07-26 07:27:04 +0000

    blk-merge: fix

--- modules/linux_adaptor/kernel_modules/block/blk-merge.c
+++ modules/linux_adaptor/kernel_modules/block/blk-merge.c
@@ -20,6 +20,27 @@
 #include "../adaptor.h"
 
 /**
+ * get_max_segment_size() - maximum number of bytes to add as a single segment
+ * @lim: Request queue limits.
+ * @paddr: address of the range to add
+ * @len: maximum length available to add at @paddr
+ *
+ * Returns the maximum number of bytes of the range starting at @paddr that can
+ * be added to a single segment.
+ */
+static inline unsigned get_max_segment_size(const struct queue_limits *lim,
+        phys_addr_t paddr, unsigned int len)
+{
+    /*
+     * Prevent an overflow if mask = ULONG_MAX and offset = 0 by adding 1
+     * after having calculated the minimum.
+     */
+    return min_t(unsigned long, len,
+        min(lim->seg_boundary_mask - (lim->seg_boundary_mask & paddr),
+            (unsigned long)lim->max_segment_size - 1) + 1);
+}
+
+/**
  * blk_attempt_plug_merge - try to merge with %current's plugged list
  * @q: request_queue new bio is being queued at
  * @bio: new bio being queued
@@ -79,6 +100,110 @@ bool blk_bio_list_merge(struct request_queue *q, struct list_head *list,
     PANIC("");
 }
 
+static inline struct scatterlist *blk_next_sg(struct scatterlist **sg,
+        struct scatterlist *sglist)
+{
+    if (!*sg)
+        return sglist;
+
+    /*
+     * If the driver previously mapped a shorter list, we could see a
+     * termination bit prematurely unless it fully inits the sg table
+     * on each mapping. We KNOW that there must be more entries here
+     * or the driver would be buggy, so force clear the termination bit
+     * to avoid doing a full sg_init_table() in drivers for each command.
+     */
+    sg_unmark_end(*sg);
+    return sg_next(*sg);
+}
+
+/* only try to merge bvecs into one sg if they are from two bios */
+static inline bool
+__blk_segment_map_sg_merge(struct request_queue *q, struct bio_vec *bvec,
+               struct bio_vec *bvprv, struct scatterlist **sg)
+{
+    PANIC("");
+}
+
+static unsigned blk_bvec_map_sg(struct request_queue *q,
+        struct bio_vec *bvec, struct scatterlist *sglist,
+        struct scatterlist **sg)
+{
+    unsigned nbytes = bvec->bv_len;
+    unsigned nsegs = 0, total = 0;
+
+    while (nbytes > 0) {
+        unsigned offset = bvec->bv_offset + total;
+        unsigned len = get_max_segment_size(&q->limits,
+                bvec_phys(bvec) + total, nbytes);
+        struct page *page = bvec->bv_page;
+
+        /*
+         * Unfortunately a fair number of drivers barf on scatterlists
+         * that have an offset larger than PAGE_SIZE, despite other
+         * subsystems dealing with that invariant just fine.  For now
+         * stick to the legacy format where we never present those from
+         * the block layer, but the code below should be removed once
+         * these offenders (mostly MMC/SD drivers) are fixed.
+         */
+        page += (offset >> PAGE_SHIFT);
+        offset &= ~PAGE_MASK;
+
+        *sg = blk_next_sg(sg, sglist);
+        sg_set_page(*sg, page, len, offset);
+
+        total += len;
+        nbytes -= len;
+        nsegs++;
+    }
+
+    return nsegs;
+}
+
+static inline int __blk_bvec_map_sg(struct bio_vec bv,
+        struct scatterlist *sglist, struct scatterlist **sg)
+{
+    *sg = blk_next_sg(sg, sglist);
+    sg_set_page(*sg, bv.bv_page, bv.bv_len, bv.bv_offset);
+    return 1;
+}
+
+static int __blk_bios_map_sg(struct request_queue *q, struct bio *bio,
+                 struct scatterlist *sglist,
+                 struct scatterlist **sg)
+{
+    struct bio_vec bvec, bvprv = { NULL };
+    struct bvec_iter iter;
+    int nsegs = 0;
+    bool new_bio = false;
+
+    for_each_bio(bio) {
+        bio_for_each_bvec(bvec, bio, iter) {
+            /*
+             * Only try to merge bvecs from two bios given we
+             * have done bio internal merge when adding pages
+             * to bio
+             */
+            if (new_bio &&
+                __blk_segment_map_sg_merge(q, &bvec, &bvprv, sg))
+                goto next_bvec;
+
+            if (bvec.bv_offset + bvec.bv_len <= PAGE_SIZE)
+                nsegs += __blk_bvec_map_sg(bvec, sglist, sg);
+            else
+                nsegs += blk_bvec_map_sg(q, &bvec, sglist, sg);
+ next_bvec:
+            new_bio = false;
+        }
+        if (likely(bio->bi_iter.bi_size)) {
+            bvprv = bvec;
+            new_bio = true;
+        }
+    }
+
+    return nsegs;
+}
+
 /*
  * map a request to scatterlist, return number of sg entries setup. Caller
  * must make sure sg can hold rq->nr_phys_segments entries
@@ -86,5 +211,21 @@ bool blk_bio_list_merge(struct request_queue *q, struct list_head *list,
 int __blk_rq_map_sg(struct request_queue *q, struct request *rq,
         struct scatterlist *sglist, struct scatterlist **last_sg)
 {
-    PANIC("");
+    int nsegs = 0;
+
+    if (rq->rq_flags & RQF_SPECIAL_PAYLOAD)
+        nsegs = __blk_bvec_map_sg(rq->special_vec, sglist, last_sg);
+    else if (rq->bio)
+        nsegs = __blk_bios_map_sg(q, rq->bio, sglist, last_sg);
+
+    if (*last_sg)
+        sg_mark_end(*last_sg);
+
+    /*
+     * Something must have been wrong if the figured number of
+     * segment is bigger than number of req's physical segments
+     */
+    WARN_ON(nsegs > blk_rq_nr_phys_segments(rq));
+
+    return nsegs;
 }
