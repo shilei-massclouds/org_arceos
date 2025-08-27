c5c20b7f4fb0a2e316c22d9d09c0f9b0c24fae89
Shi Lei <shi_lei@massclouds.com>
2025-08-27 09:40:57 +0000

    add show_stack

---
+++ modules/linux_adaptor/kernel_modules/kernel/stacktrace.c
@@ -0,0 +1,97 @@
+#include <linux/export.h>
+#include <linux/kallsyms.h>
+#include <linux/sched.h>
+#include <linux/sched/debug.h>
+#include <linux/sched/task_stack.h>
+#include <linux/stacktrace.h>
+#include <linux/ftrace.h>
+
+#include <asm/stacktrace.h>
+
+#include "../adaptor.h"
+
+static inline int fp_is_valid(unsigned long fp, unsigned long sp)
+{
+    unsigned long low, high;
+
+    low = sp + sizeof(struct stackframe);
+    high = ALIGN(sp, THREAD_SIZE);
+
+    return !(fp < low || fp > high || fp & 0x07);
+}
+
+void notrace walk_stackframe(struct task_struct *task, struct pt_regs *regs,
+                 bool (*fn)(void *, unsigned long), void *arg)
+{
+    unsigned long fp, sp, pc;
+    int graph_idx = 0;
+    int level = 0;
+
+    if (regs) {
+        fp = frame_pointer(regs);
+        sp = user_stack_pointer(regs);
+        pc = instruction_pointer(regs);
+    } else if (task == NULL || task == current) {
+        fp = (unsigned long)__builtin_frame_address(0);
+        sp = current_stack_pointer;
+        pc = (unsigned long)walk_stackframe;
+        level = -1;
+    } else {
+        /* task blocked in __switch_to */
+        fp = task->thread.s[0];
+        sp = task->thread.sp;
+        pc = task->thread.ra;
+    }
+
+    for (;;) {
+        struct stackframe *frame;
+
+        if (unlikely(!__kernel_text_address(pc) || (level++ >= 0 && !fn(arg, pc))))
+            break;
+
+        if (unlikely(!fp_is_valid(fp, sp)))
+            break;
+
+        /* Unwind stack frame */
+        frame = (struct stackframe *)fp - 1;
+        sp = fp;
+        if (regs && (regs->epc == pc) && fp_is_valid(frame->ra, sp)) {
+            PANIC("stage1");
+        } else {
+            fp = frame->fp;
+            pc = ftrace_graph_ret_addr(current, &graph_idx, frame->ra,
+                           &frame->ra);
+            /*
+            if (pc >= (unsigned long)handle_exception &&
+                pc < (unsigned long)&ret_from_exception_end) {
+                */
+                if (unlikely(!__kernel_text_address(pc) || !fn(arg, pc)))
+                    break;
+
+                pc = ((struct pt_regs *)sp)->epc;
+                fp = ((struct pt_regs *)sp)->s0;
+            //}
+        }
+    }
+}
+
+static bool print_trace_address(void *arg, unsigned long pc)
+{
+    const char *loglvl = arg;
+
+    //print_ip_sym(loglvl, pc);
+    printk("%s[<%lx>] %lx\n", loglvl, (void *) pc, (void *) pc);
+    return true;
+}
+
+noinline void dump_backtrace(struct pt_regs *regs, struct task_struct *task,
+            const char *loglvl)
+{
+    walk_stackframe(task, regs, print_trace_address, (void *)loglvl);
+}
+
+void show_stack(struct task_struct *task, unsigned long *sp, const char *loglvl)
+{
+    pr_cont("%sCall Trace:\n", loglvl);
+    dump_backtrace(NULL, task, loglvl);
+}
---
+++ modules/linux_adaptor/kernel_modules/lib/dump_stack.c
@@ -0,0 +1,95 @@
+#include <linux/kernel.h>
+#include <linux/buildid.h>
+#include <linux/export.h>
+#include <linux/sched.h>
+#include <linux/sched/debug.h>
+#include <linux/smp.h>
+#include <linux/atomic.h>
+#include <linux/kexec.h>
+#include <linux/utsname.h>
+#include <linux/stop_machine.h>
+
+#include "../adaptor.h"
+
+#if IS_ENABLED(CONFIG_STACKTRACE_BUILD_ID)
+#define BUILD_ID_FMT " %20phN"
+#define BUILD_ID_VAL vmlinux_build_id
+#else
+#define BUILD_ID_FMT "%s"
+#define BUILD_ID_VAL ""
+#endif
+
+/**
+ * dump_stack_print_info - print generic debug info for dump_stack()
+ * @log_lvl: log level
+ *
+ * Arch-specific dump_stack() implementations can use this function to
+ * print out the same debug information as the generic dump_stack().
+ */
+void dump_stack_print_info(const char *log_lvl)
+{
+    printk("%sCPU: %d UID: %u PID: %d Comm: %.20s %s " BUILD_ID_FMT "\n",
+           log_lvl, raw_smp_processor_id(),
+           __kuid_val(current_real_cred()->euid),
+           current->pid, current->comm,
+           kexec_crash_loaded() ? "Kdump: loaded " : "",
+           print_tainted(),
+           //init_utsname()->release,
+           //(int)strcspn(init_utsname()->version, " "),
+           //init_utsname()->version,
+           BUILD_ID_VAL);
+
+#if 0
+    if (get_taint())
+        printk("%s%s\n", log_lvl, print_tainted_verbose());
+
+    if (dump_stack_arch_desc_str[0] != '\0')
+        printk("%sHardware name: %s\n",
+               log_lvl, dump_stack_arch_desc_str);
+
+    print_worker_info(log_lvl, current);
+    print_stop_info(log_lvl, current);
+    print_scx_info(log_lvl, current);
+#endif
+}
+
+static void __dump_stack(const char *log_lvl)
+{
+    dump_stack_print_info(log_lvl);
+    show_stack(NULL, NULL, log_lvl);
+}
+
+/**
+ * dump_stack_lvl - dump the current task information and its stack trace
+ * @log_lvl: log level
+ *
+ * Architectures can override this implementation by implementing its own.
+ */
+asmlinkage __visible void dump_stack_lvl(const char *log_lvl)
+{
+    bool in_panic = this_cpu_in_panic();
+    unsigned long flags;
+
+    /*
+     * Permit this cpu to perform nested stack dumps while serialising
+     * against other CPUs, unless this CPU is in panic.
+     *
+     * When in panic, non-panic CPUs are not permitted to store new
+     * printk messages so there is no need to synchronize the output.
+     * This avoids potential deadlock in panic() if another CPU is
+     * holding and unable to release the printk_cpu_sync.
+     */
+    if (!in_panic)
+        printk_cpu_sync_get_irqsave(flags);
+
+    __dump_stack(log_lvl);
+
+    if (!in_panic)
+        printk_cpu_sync_put_irqrestore(flags);
+    PANIC("");
+}
+
+asmlinkage __visible void dump_stack(void)
+{
+    dump_stack_lvl(KERN_DEFAULT);
+}
