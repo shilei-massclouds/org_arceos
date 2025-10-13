fb99ec21d1ca085e5a979b2652f83bd1e82f083f
Shi Lei <shi_lei@massclouds.com>
2025-10-13 15:13:58 +0000

    arch/riscv/kernel: add it

--- modules/linux_adaptor/kernel_modules/arch/riscv/kernel/irq.c
+++ modules/linux_adaptor/kernel_modules/arch/riscv/kernel/irq.c
@@ -4,10 +4,26 @@
 #include <linux/module.h>
 #include <linux/scs.h>
 #include <linux/seq_file.h>
+#include <linux/fwnode.h>
 #include <asm/sbi.h>
 #include <asm/smp.h>
 #include <asm/softirq_stack.h>
 #include <asm/stacktrace.h>
+
+static struct fwnode_handle *(*__get_intc_node)(void);
+
+void riscv_set_intc_hwnode_fn(struct fwnode_handle *(*fn)(void))
+{
+    __get_intc_node = fn;
+}
+
+struct fwnode_handle *riscv_get_intc_hwnode(void)
+{
+    if (__get_intc_node)
+        return __get_intc_node();
+
+    return NULL;
+}
 
 void __init init_IRQ(void)
 {
