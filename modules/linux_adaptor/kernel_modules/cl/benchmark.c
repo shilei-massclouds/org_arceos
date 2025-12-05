886cb60050d38b8a7eb59be548c2dc34dd0c76f1
Shi Lei <shi_lei@massclouds.com>
2025-12-05 03:24:27 +0000

    fix benchmark

--- modules/linux_adaptor/kernel_modules/cl/benchmark.c
+++ modules/linux_adaptor/kernel_modules/cl/benchmark.c
@@ -103,7 +103,7 @@ static void read_test_file(const char *fname, int size, int count)
 
     t1 = get_ticks();
     printk("READ [size=%d, count=%d]: ticks: %llu\n",
-           size, count, (t1 - t0));
+           size, count, (t1 - t0)/count);
 
     if (filp_close(f, 0)) {
         PANIC("close dir fd err.");
@@ -120,13 +120,13 @@ static void do_test(const char *path, int size, int count)
     remove_test_file(path);
 }
 
-#if 0
-static void trace_write_file(const char *fname)
+#if 1
+static void single_write_file(const char *fname)
 {
     uint64_t t0, t1, t2, t3;
 
     struct file *f;
-    printk("%s: open for trace_write .. .\n", __func__);
+    printk("%s: open for single_write .. .\n", __func__);
     f = filp_open(fname, O_WRONLY, 0);
     if (IS_ERR(f)) {
         PANIC("bad file for write.");
@@ -173,170 +173,20 @@ void bench_ext4(void)
 {
     char *path = "/bench_file";
 
-#if 0
+    /* Single write */
+#if 1
     create_test_file(path);
-    trace_write_file(path);
+    single_write_file(path);
+
+    read_test_file(path, 64, 1);
 #endif
 
+#if 0
     do_test(path, 64, COUNT);
     do_test(path, 128, COUNT);
     do_test(path, 1024, COUNT);
     do_test(path, 4096, COUNT);
+#endif
 
     printk("Test ok!\n");
 }
-
-
-#if 0
-#include <linux/fs.h>
-#include <linux/dirent.h>
-#include <linux/blkdev.h>
-#include <linux/syscalls.h>
-
-#include "adaptor.h"
-#include "cl_syscalls.h"
-
-/* Default count 1000000 */
-#define COUNT 1000
-
-extern int cl_filp_flush(int fd);
-
-static bool
-_exists(const char *fname)
-{
-    struct stat buf;
-    int err = cl_sys_newstat(fname, &buf);
-    if (err < 0) {
-        if (err == -ENOENT) {
-            return false;
-        } else {
-            printk("stat err: %d\n", err);
-            PANIC("get file stat err.");
-        }
-    }
-    return true;
-}
-
-static void
-create_test_file(const char *fname)
-{
-    int fd = cl_sys_open(fname, O_CREAT, S_IRUSR|S_IWUSR);
-    if (fd < 0) {
-        PANIC("bad file fd.");
-    }
-    printk("prepare test file '%s' fd '%d'.\n", fname, fd);
-
-    if (cl_sys_close(fd)) {
-        PANIC("close dir fd err.");
-    }
-
-    CL_ASSERT(_exists(fname), "No file after creating it.");
-}
-
-static void
-remove_test_file(const char *fname)
-{
-    int err = cl_sys_unlink(fname);
-    if (err < 0) {
-        PANIC("remove file err.");
-    }
-
-    CL_ASSERT(!_exists(fname), "Found file after removing it.");
-}
-
-static void write_test_file(const char *fname, int size, int count)
-{
-    uint64_t t0, t1;
-
-    int fd = cl_sys_open(fname, O_WRONLY, 0);
-    if (fd < 0) {
-        printk("open for write err '%d'.\n", fd);
-        PANIC("bad dir fd.");
-    }
-
-    t0 = get_ticks();
-
-    char wbuf[4096];
-    for (int i = 0; i < count; i++) {
-        if (count % 1000 == 0) {
-            cl_sys_lseek(fd, 0, SEEK_SET);
-        }
-
-        int err = cl_sys_write(fd, wbuf, size);
-        if (err < 0) {
-            printk("write err: %d\n", err);
-            PANIC("write file err.");
-        }
-        if (cl_filp_flush(fd) != 0) {
-            PANIC("flush file err.");
-        }
-        if (cl_sys_fsync(fd) != 0) {
-            PANIC("sync file err.");
-        }
-    }
-
-    t1 = get_ticks();
-    printk("WRITE [size=%d, count=%d]: ticks: %lu\n",
-           size, count, (t1 - t0)/count);
-
-    if (cl_sys_close(fd)) {
-        PANIC("close dir fd err.");
-    }
-}
-
-static void read_test_file(const char *fname, int size, int count)
-{
-    uint64_t t0, t1;
-
-    int fd = cl_sys_open(fname, O_RDONLY, 0);
-    if (fd < 0) {
-        printk("open for write err '%d'.\n", fd);
-        PANIC("bad dir fd.");
-    }
-
-    t0 = get_ticks();
-
-    char rbuf[4096];
-    for (int i = 0; i < count; i++) {
-        if (count % 1000 == 0) {
-            cl_sys_lseek(fd, 0, SEEK_SET);
-        }
-
-        int err = cl_sys_read(fd, rbuf, size);
-        if (err < 0) {
-            printk("read err: %d\n", err);
-            PANIC("read file err.");
-        }
-    }
-
-    t1 = get_ticks();
-    printk("READ [size=%d, count=%d]: ticks: %lu\n",
-           size, count, (t1 - t0)/count);
-
-    if (cl_sys_close(fd)) {
-        PANIC("close dir fd err.");
-    }
-}
-
-static void do_test(const char *path, int size, int count)
-{
-    printk("Test [size=%d, count=%d] ...\n", size, count);
-    create_test_file(path);
-
-    write_test_file(path, size, count);
-    read_test_file(path, size, count);
-
-    remove_test_file(path);
-    printk("Test [size=%d, count=%d] ok!\n", size, count);
-}
-
-void bench_ext4(void)
-{
-    char *path = "/bench_file";
-
-    do_test(path, 64, COUNT);
-    do_test(path, 128, COUNT);
-    do_test(path, 1024, COUNT);
-    do_test(path, 4096, COUNT);
-}
-#endif
