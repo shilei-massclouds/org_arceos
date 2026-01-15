72fe2e10efe933860aef7dac6911d1b439245ab3
Shi Lei <shi_lei@massclouds.com>
2026-01-15 15:18:50 +0000

    add tests

--- repeat_wq.sh
+++ repeat_wq.sh
@@ -5,11 +5,11 @@
 TEST_NUM=100
 for ((i=0;i<$TEST_NUM;i++))
 do
-    #make ARCH=riscv64 A=examples/task/affinity run FEATURES="sched-rr" SMP=8
-    #make ARCH=riscv64 A=examples/task/irq run FEATURES="sched-rr" SMP=8
-    #make ARCH=riscv64 A=examples/task/parallel run FEATURES="sched-rr" SMP=8
-    #make ARCH=riscv64 A=examples/task/priority run FEATURES="sched-rr" SMP=8
-    #make ARCH=riscv64 A=examples/task/sleep run FEATURES="sched-rr" SMP=8
+    make ARCH=riscv64 A=examples/task/affinity run FEATURES="sched-rr" SMP=8
+    make ARCH=riscv64 A=examples/task/irq run FEATURES="sched-rr" SMP=8
+    make ARCH=riscv64 A=examples/task/parallel run FEATURES="sched-rr" SMP=8
+    make ARCH=riscv64 A=examples/task/priority run FEATURES="sched-rr" SMP=8
+    make ARCH=riscv64 A=examples/task/sleep run FEATURES="sched-rr" SMP=8
     make ARCH=riscv64 A=examples/task/wait_queue run FEATURES="sched-rr" SMP=8
-    #make ARCH=riscv64 A=examples/task/yield run FEATURES="sched-rr" SMP=8
+    make ARCH=riscv64 A=examples/task/yield run FEATURES="sched-rr" SMP=8
 done
