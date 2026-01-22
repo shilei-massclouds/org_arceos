51039867ce97f2643ec28912d1c5df73557098a0
Shi Lei <shi_lei@massclouds.com>
2026-01-22 15:07:36 +0000

    examples: add mutex test

--- repeat_wq.sh
+++ repeat_wq.sh
@@ -2,24 +2,26 @@
 
 # affinity  irq  parallel  priority  sleep  tls  wait_queue  yield
 
-TEST_NUM=100
+TEST_NUM=10
 for ((i=0;i<$TEST_NUM;i++))
 do
-    make ARCH=riscv64 A=examples/task/affinity run FEATURES="sched-rr" SMP=8
-    make ARCH=riscv64 A=examples/task/irq run FEATURES="sched-rr" SMP=8
-    make ARCH=riscv64 A=examples/task/parallel run FEATURES="sched-rr" SMP=8
-    make ARCH=riscv64 A=examples/task/priority run FEATURES="sched-rr" SMP=8
-    make ARCH=riscv64 A=examples/task/sleep run FEATURES="sched-rr" SMP=8
-    make ARCH=riscv64 A=examples/task/wait_queue run FEATURES="sched-rr" SMP=8
-    make ARCH=riscv64 A=examples/task/yield run FEATURES="sched-rr" SMP=8
-
-    make ARCH=riscv64 A=examples/tour/u_1_0/ run FEATURES="sched-rr" SMP=8
-    make ARCH=riscv64 A=examples/tour/u_2_0/ run FEATURES="sched-rr" SMP=8
-    make ARCH=riscv64 A=examples/tour/u_3_0/ run FEATURES="sched-rr" SMP=8
-    make ARCH=riscv64 A=examples/tour/u_4_0/ run FEATURES="sched-rr" SMP=8
-    make ARCH=riscv64 A=examples/tour/u_5_0/ run FEATURES="sched-rr" SMP=8
-    make ARCH=riscv64 A=examples/tour/u_6_0/ run FEATURES="sched-rr" SMP=8
-    make ARCH=riscv64 A=examples/tour/u_6_1/ run FEATURES="sched-rr" SMP=8
-    make ARCH=riscv64 A=examples/tour/u_7_0/ run FEATURES="sched-rr" SMP=8 BLK=y
-    make ARCH=riscv64 A=examples/tour/u_8_0/ run FEATURES="sched-rr" SMP=8 BLK=y
+    make ARCH=riscv64 A=examples/task/mutex run FEATURES="sched-rr" SMP=8
+    make ARCH=riscv64 A=examples/task/mutex run FEATURES="sched-cfs" SMP=8
+#    make ARCH=riscv64 A=examples/task/affinity run FEATURES="sched-rr" SMP=8
+#    make ARCH=riscv64 A=examples/task/irq run FEATURES="sched-rr" SMP=8
+#    make ARCH=riscv64 A=examples/task/parallel run FEATURES="sched-rr" SMP=8
+#    make ARCH=riscv64 A=examples/task/priority run FEATURES="sched-rr" SMP=8
+#    make ARCH=riscv64 A=examples/task/sleep run FEATURES="sched-rr" SMP=8
+#    make ARCH=riscv64 A=examples/task/wait_queue run FEATURES="sched-rr" SMP=8
+#    make ARCH=riscv64 A=examples/task/yield run FEATURES="sched-rr" SMP=8
+#
+#    make ARCH=riscv64 A=examples/tour/u_1_0/ run FEATURES="sched-rr" SMP=8
+#    make ARCH=riscv64 A=examples/tour/u_2_0/ run FEATURES="sched-rr" SMP=8
+#    make ARCH=riscv64 A=examples/tour/u_3_0/ run FEATURES="sched-rr" SMP=8
+#    make ARCH=riscv64 A=examples/tour/u_4_0/ run FEATURES="sched-rr" SMP=8
+#    make ARCH=riscv64 A=examples/tour/u_5_0/ run FEATURES="sched-rr" SMP=8
+#    make ARCH=riscv64 A=examples/tour/u_6_0/ run FEATURES="sched-rr" SMP=8
+#    make ARCH=riscv64 A=examples/tour/u_6_1/ run FEATURES="sched-rr" SMP=8
+#    make ARCH=riscv64 A=examples/tour/u_7_0/ run FEATURES="sched-rr" SMP=8 BLK=y
+#    make ARCH=riscv64 A=examples/tour/u_8_0/ run FEATURES="sched-rr" SMP=8 BLK=y
 done
