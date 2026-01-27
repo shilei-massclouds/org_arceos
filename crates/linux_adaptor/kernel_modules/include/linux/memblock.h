d7451b7ddb6b45468f2a7f9e538d1d68f6656bb8
Shi Lei <shi_lei@massclouds.com>
2026-01-27 08:20:54 +0000

    features: remove cgroup support

--- crates/linux_adaptor/kernel_modules/include/linux/memblock.h
+++ crates/linux_adaptor/kernel_modules/include/linux/memblock.h
@@ -394,7 +394,12 @@ phys_addr_t memblock_alloc_range_nid(phys_addr_t size,
 				      phys_addr_t end, int nid, bool exact_nid);
 phys_addr_t memblock_phys_alloc_try_nid(phys_addr_t size, phys_addr_t align, int nid);
 
-phys_addr_t memblock_phys_alloc(phys_addr_t size, phys_addr_t align);
+static __always_inline phys_addr_t memblock_phys_alloc(phys_addr_t size,
+						       phys_addr_t align)
+{
+	return memblock_phys_alloc_range(size, align, 0,
+					 MEMBLOCK_ALLOC_ACCESSIBLE);
+}
 
 void *memblock_alloc_exact_nid_raw(phys_addr_t size, phys_addr_t align,
 				 phys_addr_t min_addr, phys_addr_t max_addr,
@@ -406,7 +411,11 @@ void *memblock_alloc_try_nid(phys_addr_t size, phys_addr_t align,
 			     phys_addr_t min_addr, phys_addr_t max_addr,
 			     int nid);
 
-void *memblock_alloc(phys_addr_t size, phys_addr_t align);
+static __always_inline void *memblock_alloc(phys_addr_t size, phys_addr_t align)
+{
+	return memblock_alloc_try_nid(size, align, MEMBLOCK_LOW_LIMIT,
+				      MEMBLOCK_ALLOC_ACCESSIBLE, NUMA_NO_NODE);
+}
 
 static inline void *memblock_alloc_raw(phys_addr_t size,
 					       phys_addr_t align)
--- crates/linux_adaptor/kernel_modules/mm/memblock.c
+++ crates/linux_adaptor/kernel_modules/mm/memblock.c
@@ -2461,15 +2461,3 @@ __initcall(memblock_init_debugfs);
 
 #endif /* CONFIG_DEBUG_FS */
 #endif
-
-phys_addr_t memblock_phys_alloc(phys_addr_t size, phys_addr_t align)
-{
-	return memblock_phys_alloc_range(size, align, 0,
-					 MEMBLOCK_ALLOC_ACCESSIBLE);
-}
-
-void *memblock_alloc(phys_addr_t size, phys_addr_t align)
-{
-	return memblock_alloc_try_nid(size, align, MEMBLOCK_LOW_LIMIT,
-				      MEMBLOCK_ALLOC_ACCESSIBLE, NUMA_NO_NODE);
-}
