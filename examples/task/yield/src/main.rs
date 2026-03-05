6d0f19cd3f28dea5158702fa3896d5130434786f
Shi Lei <shi_lei@massclouds.com>
2026-03-05 15:30:15 +0000

    test: repeat_test.sh can be used to trigger concurrency deadlocks

---
+++ examples/task/yield/src/main.rs
@@ -0,0 +1,38 @@
+#![cfg_attr(feature = "axstd", no_std)]
+#![cfg_attr(feature = "axstd", no_main)]
+
+#[macro_use]
+#[cfg(feature = "axstd")]
+extern crate axstd as std;
+
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::thread;
+
+const NUM_TASKS: usize = 10;
+static FINISHED_TASKS: AtomicUsize = AtomicUsize::new(0);
+
+#[cfg_attr(feature = "axstd", no_mangle)]
+fn main() {
+    for i in 0..NUM_TASKS {
+        thread::spawn(move || {
+            println!("Hello, task {}! id = {:?}", i, thread::current().id());
+
+            #[cfg(all(not(feature = "sched-rr"), not(feature = "sched-cfs")))]
+            thread::yield_now();
+
+            let _order = FINISHED_TASKS.fetch_add(1, Ordering::Relaxed);
+            #[cfg(feature = "axstd")]
+            if cfg!(not(feature = "sched-cfs"))
+                && thread::available_parallelism().unwrap().get() == 1
+            {
+                assert!(_order == i); // FIFO scheduler
+            }
+        });
+    }
+    println!("Hello, main task!");
+    while FINISHED_TASKS.load(Ordering::Relaxed) < NUM_TASKS {
+        #[cfg(all(not(feature = "sched-rr"), not(feature = "sched-cfs")))]
+        thread::yield_now();
+    }
+    println!("Task yielding tests run OK!");
+}
