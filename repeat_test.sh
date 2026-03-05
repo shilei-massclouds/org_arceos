d89cbf59b7a70c7862d839509a91945962cf22eb
Shi Lei <shi_lei@massclouds.com>
2026-03-05 15:30:15 +0000

    test: repeat_test.sh can be used to trigger concurrency deadlocks

---
+++ repeat_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# affinity  irq  parallel  priority  sleep  tls  wait_queue  yield
+
+make ARCH=riscv64 defconfig
+
+TEST_NUM=1000
+for ((i=0;i<$TEST_NUM;i++))
+do
+    make ARCH=riscv64 A=examples/task/affinity run FEATURES="sched-rr" SMP=8
+    make ARCH=riscv64 A=examples/task/irq run FEATURES="sched-rr" SMP=8
+    make ARCH=riscv64 A=examples/task/parallel run FEATURES="sched-rr" SMP=8
+    make ARCH=riscv64 A=examples/task/priority run FEATURES="sched-rr" SMP=8
+    make ARCH=riscv64 A=examples/task/sleep run FEATURES="sched-rr" SMP=8
+    make ARCH=riscv64 A=examples/task/wait_queue run FEATURES="sched-rr" SMP=8
+    make ARCH=riscv64 A=examples/task/yield run FEATURES="sched-rr" SMP=8
+done
