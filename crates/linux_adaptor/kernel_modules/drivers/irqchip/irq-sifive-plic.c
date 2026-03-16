fab0827c51d450c5f58506855b1dc5d298c3b06c
Shi Lei <shi_lei@massclouds.com>
2026-03-16 03:43:03 +0000

    driver:irq-plic: add it

--- crates/linux_adaptor/kernel_modules/drivers/irqchip/irq-sifive-plic.c
+++ crates/linux_adaptor/kernel_modules/drivers/irqchip/irq-sifive-plic.c
@@ -21,8 +21,6 @@
 #include <linux/syscore_ops.h>
 #include <asm/smp.h>
 
-#include "adaptor.h"
-
 /*
  * This driver implements a version of the RISC-V PLIC with the actual layout
  * specified in chapter 8 of the SiFive U5 Coreplex Series Manual:
@@ -51,8 +49,6 @@
 #define CONTEXT_ENABLE_BASE		0x2000
 #define     CONTEXT_ENABLE_SIZE		0x80
 
-#define PENDING_BASE			0x1000
-
 /*
  * Each hart context has a set of control registers associated with it.  Right
  * now there's only two: a source priority threshold over which the hart will
@@ -67,7 +63,6 @@
 #define	PLIC_ENABLE_THRESHOLD		0
 
 #define PLIC_QUIRK_EDGE_INTERRUPT	0
-#define PLIC_QUIRK_CLAIM_REGISTER	1
 
 struct plic_priv {
 	struct fwnode_handle *fwnode;
@@ -289,7 +284,7 @@ static void plic_irq_resume(void)
 
 	priv = per_cpu_ptr(&plic_handlers, smp_processor_id())->priv;
 
-	for (i = 1; i < priv->nr_irqs; i++) {
+	for (i = 0; i < priv->nr_irqs; i++) {
 		index = BIT_WORD(i);
 		writel((priv->prio_save[index] & BIT_MASK(i)) ? 1 : 0,
 		       priv->regs + PRIORITY_BASE + i * PRIORITY_PER_ID);
@@ -371,82 +366,6 @@ static const struct irq_domain_ops plic_irqdomain_ops = {
 	.free		= irq_domain_free_irqs_top,
 };
 
-static bool plic_check_enable_first_pending(u32 ie[])
-{
-	struct plic_handler *handler = this_cpu_ptr(&plic_handlers);
-	void __iomem *enable = handler->enable_base;
-	void __iomem *pending = handler->priv->regs + PENDING_BASE;
-	int nr_irqs = handler->priv->nr_irqs;
-	int nr_irq_groups = (nr_irqs + 31) / 32;
-	bool is_pending = false;
-	int i, j;
-
-	raw_spin_lock(&handler->enable_lock);
-
-	// Read current interrupt enables
-	for (i = 0; i < nr_irq_groups; i++)
-		ie[i] = readl(enable + i * sizeof(u32));
-
-	// Check for pending interrupts and enable only the first one found
-	for (i = 0; i < nr_irq_groups; i++) {
-		u32 pending_irqs = readl(pending + i * sizeof(u32)) & ie[i];
-
-		if (pending_irqs) {
-			int nbit = __ffs(pending_irqs);
-
-			for (j = 0; j < nr_irq_groups; j++)
-				writel((i == j)?(1 << nbit):0, enable + j * sizeof(u32));
-			is_pending = true;
-			break;
-		}
-	}
-
-	raw_spin_unlock(&handler->enable_lock);
-
-	return is_pending;
-}
-
-static void plic_restore_enable_state(u32 ie[])
-{
-	struct plic_handler *handler = this_cpu_ptr(&plic_handlers);
-	void __iomem *enable = handler->enable_base;
-	int nr_irqs = handler->priv->nr_irqs;
-	int nr_irq_groups = (nr_irqs + 31) / 32;
-	int i;
-
-	raw_spin_lock(&handler->enable_lock);
-
-	for (i = 0; i < nr_irq_groups; i++)
-		writel(ie[i], enable + i * sizeof(u32));
-
-	raw_spin_unlock(&handler->enable_lock);
-}
-
-static irq_hw_number_t plic_get_hwirq(void)
-{
-	struct plic_handler *handler = this_cpu_ptr(&plic_handlers);
-	struct plic_priv *priv = handler->priv;
-	void __iomem *claim = handler->hart_base + CONTEXT_CLAIM;
-	irq_hw_number_t hwirq;
-	u32 ie[32] = {0};
-
-	/*
-	 * Due to the implementation of the claim register in the UltraRISC DP1000
-	 * platform PLIC not conforming to the specification, this is a hardware
-	 * bug. Therefore, when an interrupt is pending, we need to disable the other
-	 * interrupts before reading the claim register. After processing the interrupt,
-	 * we should then restore the enable register.
-	 */
-	if (test_bit(PLIC_QUIRK_CLAIM_REGISTER, &priv->plic_quirks)) {
-		hwirq = plic_check_enable_first_pending(ie)?readl(claim):0;
-		plic_restore_enable_state(ie);
-	} else {
-		hwirq = readl(claim);
-	}
-
-	return hwirq;
-}
-
 /*
  * Handling an interrupt is a two-step process: first you claim the interrupt
  * by reading the claim register, then you complete the interrupt by writing
@@ -457,13 +376,14 @@ static void plic_handle_irq(struct irq_desc *desc)
 {
 	struct plic_handler *handler = this_cpu_ptr(&plic_handlers);
 	struct irq_chip *chip = irq_desc_get_chip(desc);
+	void __iomem *claim = handler->hart_base + CONTEXT_CLAIM;
 	irq_hw_number_t hwirq;
 
 	WARN_ON_ONCE(!handler->present);
 
 	chained_irq_enter(chip, desc);
 
-	while ((hwirq = plic_get_hwirq())) {
+	while ((hwirq = readl(claim))) {
 		int err = generic_handle_domain_irq(handler->priv->irqdomain,
 						    hwirq);
 		if (unlikely(err)) {
@@ -493,7 +413,6 @@ static int plic_starting_cpu(unsigned int cpu)
 {
 	struct plic_handler *handler = this_cpu_ptr(&plic_handlers);
 
-    printk("---------------- %s: cpu[%u]\n", __func__, cpu);
 	if (plic_parent_irq)
 		enable_percpu_irq(plic_parent_irq,
 				  irq_get_trigger_type(plic_parent_irq));
@@ -512,8 +431,6 @@ static const struct of_device_id plic_match[] = {
 	  .data = (const void *)BIT(PLIC_QUIRK_EDGE_INTERRUPT) },
 	{ .compatible = "thead,c900-plic",
 	  .data = (const void *)BIT(PLIC_QUIRK_EDGE_INTERRUPT) },
-	{ .compatible = "ultrarisc,dp1000-plic",
-	  .data = (const void *)BIT(PLIC_QUIRK_CLAIM_REGISTER) },
 	{}
 };
 
@@ -710,8 +627,10 @@ static int plic_probe(struct fwnode_handle *fwnode)
 
 		handler->enable_save = kcalloc(DIV_ROUND_UP(nr_irqs, 32),
 					       sizeof(*handler->enable_save), GFP_KERNEL);
-		if (!handler->enable_save)
+		if (!handler->enable_save) {
+			error = -ENOMEM;
 			goto fail_cleanup_contexts;
+		}
 done:
 		for (hwirq = 1; hwirq <= nr_irqs; hwirq++) {
 			plic_toggle(handler, hwirq, 0);
@@ -723,8 +642,10 @@ done:
 
 	priv->irqdomain = irq_domain_create_linear(fwnode, nr_irqs + 1,
 						   &plic_irqdomain_ops, priv);
-	if (WARN_ON(!priv->irqdomain))
+	if (WARN_ON(!priv->irqdomain)) {
+		error = -ENOMEM;
 		goto fail_cleanup_contexts;
+	}
 
 	/*
 	 * We can have multiple PLIC instances so setup global state
@@ -751,12 +672,9 @@ done:
 			if (plic_parent_irq)
 				irq_set_chained_handler(plic_parent_irq, plic_handle_irq);
 
-            printk("%s: ++++++++++++++++++++++++++\n", __func__);
 			cpuhp_setup_state(CPUHP_AP_IRQ_SIFIVE_PLIC_STARTING,
 					  "irqchip/sifive/plic:starting",
 					  plic_starting_cpu, plic_dying_cpu);
-            printk("%s: --------------------------\n", __func__);
-
 			register_syscore_ops(&plic_irq_syscore_ops);
 			plic_global_setup_done = true;
 		}
@@ -808,12 +726,7 @@ static struct platform_driver plic_driver = {
 	},
 	.probe = plic_platform_probe,
 };
-
-static int __init plic_init(void)
-{
-	return platform_driver_register(&plic_driver);
-}
-arch_initcall(plic_init);
+builtin_platform_driver(plic_driver);
 
 static int __init plic_early_probe(struct device_node *node,
 				   struct device_node *parent)
@@ -821,5 +734,4 @@ static int __init plic_early_probe(struct device_node *node,
 	return plic_probe(&node->fwnode);
 }
 
-IRQCHIP_DECLARE(thead_c900_plic, "thead,c900-plic", plic_early_probe);
 IRQCHIP_DECLARE(riscv, "allwinner,sun20i-d1-plic", plic_early_probe);
