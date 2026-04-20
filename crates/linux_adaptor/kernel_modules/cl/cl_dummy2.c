48d93dbf77cd2b8e10e2a8583705087fc353fb0a
Shi Lei <shi_lei@massclouds.com>
2026-04-20 08:02:27 +0000

    ipv6:icmp: add

--- crates/linux_adaptor/kernel_modules/cl/cl_dummy2.c
+++ crates/linux_adaptor/kernel_modules/cl/cl_dummy2.c
@@ -1295,3 +1295,24 @@ int __net_init xfrm_sysctl_init(struct net *net)
     pr_dummy("--> NOTE: %s: No impl.\n", __func__);
     return 0;
 }
+
+// net/ipv6/anycast.c
+int __net_init ac6_proc_init(struct net *net)
+{
+    pr_dummy("--> NOTE: %s: No impl.\n", __func__);
+    return 0;
+}
+
+int proc_dou8vec_minmax(const struct ctl_table *table, int write,
+            void *buffer, size_t *lenp, loff_t *ppos)
+{
+    pr_dummy("--> NOTE: %s: No impl.\n", __func__);
+    return 0;
+}
+
+int proc_do_large_bitmap(const struct ctl_table *table, int write,
+             void *buffer, size_t *lenp, loff_t *ppos)
+{
+    pr_dummy("--> NOTE: %s: No impl.\n", __func__);
+    return 0;
+}
